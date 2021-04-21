7307b568f6c15dfc787d6e661e24e386b636c3fe
Jorge.Marin <jorge_marin@live.com>
2021-04-21 00:00:26 -0500

    Se agregan los detalles de las peliculas

--- src/app/components/detalle/detalle.component.ts
+++ src/app/components/detalle/detalle.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MoviesService } from 'src/app/services/movies.service';
+import { Cast, PeliculaDetalle } from '../../interfaces/interfaces';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-detalle',
@@ -8,15 +10,32 @@ import { MoviesService } from 'src/app/services/movies.service';
 })
 export class DetalleComponent implements OnInit {
   @Input() id: string;
-  constructor(private moviesService: MoviesService) { }
+  pelicula: PeliculaDetalle ;
+  actores: Cast[] = [];
+  oculto = 150;
+
+  slideOptActores = {
+    slidesPerView: 3.3,
+    freeMode: true,
+    spaceBetween: -5
+  };
+
+  constructor(private moviesService: MoviesService, private modalCtrl: ModalController) { }
 
   ngOnInit() {
     this.moviesService.getPeliculaDetalle(this.id).subscribe( (response) => {
       console.log('pilla -> ', response);
+      this.pelicula = response;
     });
     this.moviesService.getActoresPelicula(this.id).subscribe( (response) => {
       console.log('pilla -> ', response);
+      this.actores = response.cast;
     });
   }
+
+  regresar() {
+    this.modalCtrl.dismiss();
+  }
+  favorito() {}
 
 }
